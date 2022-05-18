a44f584eab49b6f67878029aefb8de8b71babe8f
Дмитрий <mitya.kulik@bk.ru>
2022-05-18 13:07:11 +0300

    Готовый код

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Программа которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа");
+Console.WriteLine("Введите число: ");
+int n;
+n = Convert.ToInt32(Console.ReadLine()); 
+int b = n /10;
+int a = b%10;
+Console.WriteLine(a);
